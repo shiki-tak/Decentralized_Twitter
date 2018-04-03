16c442e0cfefc319a1b29733b58ea2f99246fe7d
shiki-tak <shiki.tak@gmail.com>
2018-04-03 12:42:28 +0900

    add form blog post

---
+++ src/components/CreateArticle.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="createarticle">
+    <div class="content">
+      <h3>Create Article</h3>
+      <p v-if="contractAddress">The contract is deployed at {{contractAddress}}</p>
+      <p v-if="!contractAddress">No contracts found</p>
+      <h5>title</h5>
+      <input v-model="title">
+      <br>
+      <h5>content</h5>
+      <input v-model="content">
+      <br>
+      <!-- <button @click="createArticle">Create</button> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import Web3 from 'web3'
+import contract from 'truffle-contract'
+import artifacts from '../../build/contracts/CryptoArticleToken.json'
+const CryptoArticle = contract(artifacts)
+
+export default {
+  name: 'CreateArticle',
+  data() {
+    return {
+      message: null,
+      contractAddress: null,
+      title: null,
+      content: null
+    }
+  },
+  created() {
+    CryptoArticle.setProvider(web3.currentProvider)
+    web3.eth.getAccounts((err, accs) => {
+      if (err != null) {
+        console.log(err)
+        this.message = "There was an error fetching your accounts. Do you have Metamask, Mist installed or an Ethereum node running? If not, you might want to look into that"
+        return
+      }
+
+      if (accs.length == 0) {
+        this.message = "Couldn't get any accounts! Make sure your Ethereum client is configured correctly."
+        return
+      }
+      this.account = accs[0];
+      CryptoArticle.deployed()
+        .then((instance) => instance.address)
+        .then((address) => {
+          this.contractAddress = address
+        })
+    })
+  }
+}
+</script>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="blog">
     <h3>This is an blog page</h3>
+    <CreateArticle/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import CreateArticle from '@/components/CreateArticle.vue'
+
+export default {
+  name: 'blog',
+  components: {
+    CreateArticle
+  }
+}
+</script>
