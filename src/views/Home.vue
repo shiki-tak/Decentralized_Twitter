0e6ddaad39f9de6899ee28ffd7612fedf8277559
shiki-tak <shiki.tak@gmail.com>
2018-04-05 18:50:01 +0900

    edit home css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,20 +24,32 @@
           <p>まず、Google Chrome用のプラグインである<a href="https://metamask.io/">MetaMask</a>をインストールします。</p>
           <p>MetaMaskはEthereumを使った分散型アプリケーション(DApps)を簡単に使うことができるプラグインであり、現在開発されているDAppsはほとんどMetaMaskを使って動作する用に作られています。</p>
           <p>インストールが完了したら、ネットワークをRopsten Test Networkに設定します。</p>
-          <img src="../assets/network_state.png" style="width: 300px; height: auto; margin-bottom: 30px"/>
+          <img src="../assets/network_state.png" style="width: 300px; height: auto;; box-shadow: 2px 2px 5px 2px #ddd;"/>
+        </ul>
+      </div>
+      <div class="buy eth">
+        <h2>2. Buy ETH</h2>
+        <ul>
+          <p>DTweetするためのETHを手に入れます。</p>
+          <p>MetaMaskから"BUY"ボタンをクリックします。</p>
+          <img src="../assets/buy.png" style="width: 300px; height: auto;"/>
+          <p>"ROPSTEN TEST FAUCET"をクリックすると、MetaMask Ether Faucetが開きます。</p>
+          <img src="../assets/buy_confirm.png" style="width: 300px; height: auto;"/>
+          <p>"request 1 ether from faucet"をクリックすると、テスト用のETHが手に入ります。</p>
+          <img src="../assets/faucet.png" style="width: 700px; height: auto;"/>
         </ul>
       </div>
       <div class="send dtweet">
-        <h2>2. Send DTweet</h2>
+        <h2>3. Send DTweet</h2>
         <ul>
           <p>DTweetタブからDTweetのTitleとContentを入力します。</p>
-          <img src="../assets/dtweet.png" style="width: 600px; height: auto; margin-bottom: 30px"/>
+          <img src="../assets/dtweet.png" style="width: 600px; height: auto;"/>
           <p>MetaMaskの"CONFIRM TRANSACTION"が開くので、"SUBMIT"をクリックします。</p>
-          <img src="../assets/tx_metamask.png" style="width: auto; height: 300px; margin-bottom: 30px"/>
+          <img src="../assets/tx_metamask.png" style="width: auto; height: 300px;"/>
           <p>ネットワークの状態にもよりますが、30秒ほどでTransactionが承認され、DTweetが表示されます。</p>
-          <img src="../assets/dtweet_result.png" style="width: auto; height: 300px; margin-bottom: 30px"/>
+          <img src="../assets/dtweet_result.png" style="width: auto; height: 300px;"/>
           <p>Transaction HashのリンクをクリックするとRopsten Test NetのEtherscanが開きます。</p>
-          <img src="../assets/ropsten_res.png" style="width: 700px; height: auto; margin-bottom: 30px"/>
+          <img src="../assets/ropsten_res.png" style="width: 700px; height: auto;"/>
         </ul>
       </div>
     </div>
@@ -51,7 +63,7 @@
 export default {
   name: 'home',
   components: {
-    
+
   }
 }
 </script>
@@ -76,6 +88,10 @@ export default {
   margin-top: 60px;
 }
 
+img {
+  margin-bottom: 30px;
+  box-shadow: 2px 2px 5px 2px #ddd;
+}
 .send.dtweet {
   margin-top: 40px;
 }
